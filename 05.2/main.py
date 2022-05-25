86ecf2f5abe0008fe8faaf85decbcdbd47373b1b
Brazuzan <eltonrosa@gmail.com>
2022-05-25 15:44:38 -0300

    05

---
+++ 05.2/main.py
@@ -0,0 +1,67 @@
+import os
+def handle_lines(lines: list[str]) -> list:
+    vents = []
+    for line in lines:
+        line = line.rstrip("\n")
+        line = line.replace(" -> ", ",")
+        line = line.split(",")
+        line = [int(x) for x in line]
+        origin = (line[0], line[1])
+        end = (line[2], line[3])
+
+        vent = (origin, end)
+
+        vents.append(vent)
+  
+    return vents
+
+def vents_paths(vents: list[tuple[tuple[int]]]):
+    points = []
+    for vent in vents:
+        straight_line = None
+
+        # Check if it is a point
+        if vent[0] == vent[1]:
+            straight_line = vent[0]
+
+        # Check for horizontal line
+        elif vent[0][1] == vent[1][1]:
+            if vent[0][0] < vent[1][0]:
+                step = 1
+            else:
+                step = -1
+            straight_line = [(x, vent[0][1]) for x in range(vent[0][0], vent[1][0] + step, step)]
+
+        # Check for vertical line
+        elif vent[0][0] == vent[1][0]:
+            if vent[0][1] < vent[1][1]:
+                step = 1
+            else:
+                step = -1
+            
+            straight_line = [(vent[0][0], y) for y in range(vent[0][1], vent[1][1] + step, step)]
+        
+        if straight_line:
+            for point in straight_line:
+                points.append(point)
+    
+    # This solution with sets is considerably faster.
+    duplicated_points = set()
+    checked_points = set()
+    for point in points:
+        if point not in checked_points:
+            checked_points.add(point)
+        else:
+            duplicated_points.add(point)
+    
+    print(len(duplicated_points))
+
+if __name__ == "__main__":
+
+    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+    with open(os.path.join(__location__, "input"), "r") as f:
+        lines = f.readlines()
+
+    processed_input = handle_lines(lines)
+    vents_paths(processed_input)
---
+++ 06.1/main.py
@@ -0,0 +1,11 @@
+import os
+
+def handle_lines(lines: list[str]) -> list:
+    pass
+
+if __name__ == "__main__":
+
+    cwd = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+    
+    with open(os.path.join(cwd, "input"), "r") as f:
+        lines = f.readlines()
